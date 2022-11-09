d073b48aba1def1cd2550ec7592b7ae9813592ce
dowel015 <dowel015@umn.edu>
2022-11-08 18:43:47 -0600

    Create function to play as computer

--- rps.js
+++ rps.js
@@ -7,4 +7,21 @@ if((input.toLowerCase() != "rock") & (input.toLowerCase() != "paper") & (input.t
 }
 
 //get computer input
+function getComputerChoice () {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    console.log(computerChoice);
+    if (computerChoice == 1) {
+        computerChoice = "rock";
+    }
+    else if (computerChoice == 2) {
+        computerChoice = "paper";
+    }
+    else {
+        computerChoice = "scissors";
+    }
+    console.log(computerChoice);
+    return computerChoice;
+}
 //play the game
+
+getComputerChoice();
